16f4025bbbc75c53b971b76ce5e6451a1c70386e
osamafayyaz3 <osama85@my.yorku.ca>
2020-05-01 16:45:29 -0400

    added styling

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,18 @@
 const path = require('path');
 const express = require("express");
-
+const rootDir = require('../util/path');
 const router = express.Router();
 
+const products = [];
+
 router.get("/add-product", (req, res) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 });
 
 router.post("/add-product", (req, res) => {
-  console.log(req.body);
+  products.push({title: req.body.title});
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const express = require("express");
-
+const rootDir = require('../util/path')
 const router = express.Router();
+const adminData = require('./admin');
 
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    console.log('shop.js', adminData.products);
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
   });
 
 
