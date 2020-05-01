9f0d08334813109ff31ab4786517b309578b6c7a
osamafayyaz3 <osama85@my.yorku.ca>
2020-05-01 15:18:33 -0400

    served html files

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const path = require('path');
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -14,7 +15,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', 'error.html'))
 });
 
 app.listen(3000);
